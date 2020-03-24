fd0bb9d792406d9726b198ed0b0f85ce76d19c11
Timtim <timkrow@centurylink.net>
2020-03-23 21:03:00 -0400

    Initial commit

---
+++ src/Animal.java
@@ -0,0 +1,7 @@
+
+public class Animal {
+
+	//Data members
+	String name;
+	
+}
---
+++ src/AnimalClient.java
@@ -0,0 +1,11 @@
+
+public class AnimalClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Create a new Animal Object
+		Animal myAnimal = new Animal();
+	}
+
+}
